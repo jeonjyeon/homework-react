51cab6351f42b2f5941ad3dcb9ceaf7c09ddc7dc
jeonjyeon <peacheese17@gmail.com>
2025-08-24 23:24:10 +0900

    ⚙️ init: 2주차 과제 프로젝트 세팅

---
+++ week2/src/app.jsx
@@ -0,0 +1,11 @@
+import { LearnSection } from '@/components'
+
+export default function App() {
+  return (
+    <LearnSection title="상태가 있는 컴포넌트" showTitle={false}>
+      <h2 className="font-[Spoqa_Han_Sans_Neo] font-bold text-2xl">
+        2주차 과제: 상태가 있는 컴포넌트 만들기
+      </h2>
+    </LearnSection>
+  )
+}
