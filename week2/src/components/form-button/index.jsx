f69c6137fc2df1883b787cdfe0d36ff2b2c06cf6
jeonjyeon <peacheese17@gmail.com>
2025-08-25 01:31:18 +0900

    💄 style: 스타일링 수정

--- week2/src/components/form-button/index.jsx
+++ week2/src/components/form-button/index.jsx
@@ -2,7 +2,7 @@ export default function FormButton({ children }) {
   return (
     <button
       type="submit"
-      className="w-full flex flex-col justify-center items-center self-stretch px-0 py-3 rounded-[999px] left-8 top-6 bg-[#3578FF]"
+      className="w-full flex flex-col justify-center items-center self-stretch mt-6 px-0 py-3 rounded-[999px] left-8 top-6 bg-[#3578FF] text-white"
     >
       {children}
     </button>
--- week2/src/components/sign-in/index.jsx
+++ week2/src/components/sign-in/index.jsx
@@ -6,7 +6,7 @@ export default function SignIn() {
     <>
       <h2 className="font-[Spoqa_Han_Sans_Neo] font-bold text-2xl">Sign In</h2>
       <div className="relative w-full h-60 flex flex-col items-center gap-[-24px] opacity-100 p-0">
-        <form className="font-[Spoqa_Han_Sans_Neo] w-full h-[200px] flex flex-col gap-y-3 gap-x-3 pt-6 pb-16 px-6 rounded-lg left-0 top-0 bg-white">
+        <form className="font-[Spoqa_Han_Sans_Neo] w-full h-[220px] flex flex-col gap-y-3 gap-x-3 pt-6 pb-16 px-6 rounded-lg left-0 top-0 bg-white">
           <FormInput type="email" />
           <FormInput type="password" />
           <FormButton>로그인</FormButton>
