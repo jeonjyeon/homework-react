996a064f4988b15b9a100a7aa8cfdc16a4d1df91
jeonjyeon <peacheese17@gmail.com>
2025-09-01 00:33:41 +0900

    ♿️ accessibility: TogglePasswordVisibilityButton에 aria-label 추가

--- week2/src/components/toggle-password-visibilty-button/index.jsx
+++ week2/src/components/toggle-password-visibilty-button/index.jsx
@@ -6,6 +6,7 @@ export default function TogglePasswordVisibilityButton({ show, onClick }) {
       type="button"
       onClick={onClick}
       className="absolute right-2 top-1/2 -translate-y-1/2"
+      aria-label={show ? '비밀번호 숨기기' : '비밀번호 보이기'}
     >
       {show ? <EyeOnIcon /> : <EyeOffIcon />}
     </button>
